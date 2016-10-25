c07d77999086815ce7116b3f8459001bfa20f83d
Nicholas Mayne <nmayne@gmail.com>
2016-10-25 01:41:05 -0700

    Add hall fbx, it's not importing correctly though. Add mouse look script add ball firing script.

---
+++ Assets/BallShooter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallShooter : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject reticle;
+    
+    private bool isFiring = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            if(!isFiring)
+            {
+                shootBall();
+            }
+            isFiring = true;
+        }
+        else
+        {
+            isFiring = false;
+        }
+	}
+
+    void shootBall()
+    {
+        var newBall = Instantiate(ball);
+        newBall.transform.position = gameObject.transform.position;
+        Vector3 shoot = (reticle.transform.position - gameObject.transform.position).normalized;
+        newBall.GetComponent<Rigidbody>().AddForce(shoot * 500.0f);
+    }
+}

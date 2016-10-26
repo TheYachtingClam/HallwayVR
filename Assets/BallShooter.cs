a022d53c01173a1fac088fee4dd0ec7a36d8b65f
Nicholas Mayne <nmayne@gmail.com>
2016-10-26 00:46:15 -0700

    Add HUD to display swipe values.

--- Assets/BallShooter.cs
+++ Assets/BallShooter.cs
@@ -1,23 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BallShooter : MonoBehaviour {
 
     public GameObject ball;
     public GameObject reticle;
-    public float speed = 500.0f;
+    public float lowSpeed = 500.0f;
+    public float highSpeed = 2500.0f;
+    public Text SwipeLeft;
+    public Text SwipeRight;
+    public Text SwipeUp;
+    public Text SwipeDown;
     
     private bool isFiring = false;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+	void Start ()
     {
-	    if(Input.GetMouseButtonDown(0))
+        var swipeController = gameObject.GetComponent<SwipeControl>();
+        swipeController.SwipeLeft += SwipeController_SwipeLeft;
+        swipeController.SwipeRight += SwipeController_SwipeRight;
+        swipeController.SwipeUp += SwipeController_SwipeUp;
+        swipeController.SwipeDown += SwipeController_SwipeDown;
+    }
+
+    void OnDestroy()
+    {
+        var swipeController = gameObject.GetComponent<SwipeControl>();
+        swipeController.SwipeLeft -= SwipeController_SwipeLeft;
+        swipeController.SwipeRight -= SwipeController_SwipeRight;
+        swipeController.SwipeUp -= SwipeController_SwipeUp;
+        swipeController.SwipeDown -= SwipeController_SwipeDown;
+    }
+
+    private void SwipeController_SwipeDown(float distance)
+    {
+        SwipeLeft.text = "0";
+        SwipeRight.text = "0";
+        SwipeUp.text = "0";
+        SwipeDown.text = distance.ToString();
+    }
+
+    private void SwipeController_SwipeUp(float distance)
+    {
+        SwipeLeft.text = "0";
+        SwipeRight.text = "0";
+        SwipeUp.text = distance.ToString();
+        SwipeDown.text = "0";
+    }
+
+    private void SwipeController_SwipeRight(float distance)
+    {
+        SwipeLeft.text = "0";
+        SwipeRight.text = distance.ToString();
+        SwipeUp.text = "0";
+        SwipeDown.text = "0";
+    }
+
+    private void SwipeController_SwipeLeft(float distance)
+    {
+        SwipeLeft.text = distance.ToString();
+        SwipeRight.text = "0";
+        SwipeUp.text = "0";
+        SwipeDown.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	    if(Input.GetMouseButton(0))
         {
             if(!isFiring)
             {
@@ -36,6 +88,6 @@ public class BallShooter : MonoBehaviour {
         var newBall = Instantiate(ball);
         newBall.transform.position = gameObject.transform.position;
         Vector3 shoot = (reticle.transform.position - gameObject.transform.position).normalized;
-        newBall.GetComponent<Rigidbody>().AddForce(shoot * speed);
+        newBall.GetComponent<Rigidbody>().AddForce(shoot * 1500.0f);
     }
 }
---
+++ Assets/Scripts/SwipeControl.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class SwipeControl : MonoBehaviour
+{
+    // After how long swipe should detect
+    public float _swipeTolerance = 20;
+
+    // Keeping initial position of finger/mouse
+    Vector3 _initialPosition = Vector3.zero;
+
+    // Will be calculate and compare with _swipeTolerance at Runtime
+    float _swipeDistance = 0;
+
+    // Determine swipe direction
+    bool _swipedHorizontal = false;
+
+    // Change in finger X position
+    float _dx = 0;
+
+    // Change in finger Y position
+    float _dy = 0;
+
+    public delegate void SwipeHandler(float distance);
+    public event SwipeHandler SwipeLeft;
+    public event SwipeHandler SwipeRight;
+    public event SwipeHandler SwipeUp;
+    public event SwipeHandler SwipeDown;
+
+    void Awake()
+    {
+        // This will keep alive this GameObject through out all the scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Finger/click Start
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            // Saving initial touch/click position
+            _initialPosition = Input.mousePosition;
+
+            // Finger/click drag
+        }
+        else if (Input.GetMouseButton(0))
+        {
+
+            // Calculating change in X between initial and current X position
+            _dx = _initialPosition.x - Input.mousePosition.x;
+
+            // Calculating change in Y between initial and current Y position
+            _dy = _initialPosition.y - Input.mousePosition.y;
+
+            // Calculating swipe distance because it may be too short and should not execute
+            _swipeDistance = Mathf.Sqrt(Mathf.Pow(_dx, 2) + Mathf.Pow(_dy, 2));
+
+            // If change in X is greater than change in Y that means user swiped Horizontally
+            _swipedHorizontal = Mathf.Abs(_dx) > Mathf.Abs(_dy);
+
+            //Finger/click lift up.
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            print("Swipe distance: " + _swipeDistance);
+            // Checking finger/click travel distance to the tolerance value.
+            if (_swipeDistance > _swipeTolerance) {
+
+                // Swiped left. As it is Horizontal and change in X is positive. Example 0 - (-5) would be +10 or 10
+                if (_swipedHorizontal && _dx > 0) {
+                    print("Swiped left");
+                    if(SwipeLeft != null)
+                        SwipeLeft(_swipeDistance);
+
+                    // Swiped Right
+                } else if (_swipedHorizontal && _dx < 0) {
+                    print("Swiped Right");
+                    if (SwipeRight != null)
+                        SwipeRight(_swipeDistance);
+
+                    // Swiped Down
+                } else if (!_swipedHorizontal && _dy > 0) {
+                    print("Swiped Down");
+                    if (SwipeDown != null)
+                        SwipeDown(_swipeDistance);
+
+                    // Swiped Up
+                } else if (!_swipedHorizontal && _dy < 0) {
+                    print("Swiped Up");
+                    if (SwipeUp != null)
+                        SwipeUp(_swipeDistance);
+                }
+            }
+
+            // Resetting values
+            _dx = 0;
+            _dy = 0;
+            _initialPosition = Vector3.zero;
+        }
+    }
+
+}
